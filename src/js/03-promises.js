a059846b99ae9cd9984b65d06dab655140e09b7e
GodOfOni <118010221+GodOfOni@users.noreply.github.com>
2023-09-24 13:38:15 +0200

    fix

---
+++ src/js/03-promises.js
@@ -0,0 +1,43 @@
+import Notiflix from 'notiflix';
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  if (shouldResolve) {
+    // Fulfill
+    return Promise.resolve({ position, delay });
+  } else {
+    // Reject
+    return Promise.reject({ position, delay });
+  }
+}
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const { delay, step, amount } = e.currentTarget;
+  let delayValue = Number(delay.value);
+  let stepValue = Number(step.value);
+  let amountValue = Number(amount.value);
+
+  for (let i = 1; i <= amountValue; i++) {
+    createPromise(i, delayValue)
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        }, delay);
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        }, delay);
+      });
+    delayValue += stepValue;
+  }
+
+  e.currentTarget.reset();
+});
